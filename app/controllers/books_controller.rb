e598cd44ca1e26b10777d19e499fe7d7bd56a751
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-17 14:07:24 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,35 @@
 class BooksController < ApplicationController
   def index
+    # book.new定義しないとerr。要確認
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
-
-  def edit
+  
+  def new
+    @book = Book.new
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
-  
+
+  def edit
+
+  end
+
+
+
   def destroy
-    
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
