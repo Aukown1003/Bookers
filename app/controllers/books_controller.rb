44c85031fa7ca00dca9b922ad5ba7cc8f7fe5788
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-18 00:38:30 +0000

    [Add]編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,11 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
@@ -20,12 +20,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
 
-
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
