b5c9f4141652b6674852f2e33fab8925c317932a
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-18 07:33:42 +0000

    [Add]フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
+    flash[:notice] = "Posted successfully"
     redirect_to book_path(@book.id)
   end
 
@@ -22,6 +23,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Update successfully"
     redirect_to book_path(book.id)
   end
 
